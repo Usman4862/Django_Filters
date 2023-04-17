1176ee70a3cba0e2b5dea34a21ead0504b6d8418
Usman4862 <um4862@gmail.com>
2023-04-17 22:36:17 +0500

    Modify Indentation, Add post method, rename Test

--- Forms/instructors/views.py
+++ Forms/instructors/views.py
@@ -4,21 +4,23 @@ from .forms import Instructor
 
 def instructor(request):
     if request.method == 'POST':
-        obj = Instructor()
+        obj = Instructor(request.POST)
         if obj.is_valid():
             print('Data Validated')
             name = obj.cleaned_data['name']
             email = obj.cleaned_data['email']
             password = obj.cleaned_data['password']
             print('Name', name)
-            print('Test', password)
+            print('Password', password)
             print('Email', email)
         else:
             print('Data not Validated')
 
     else:
         obj = Instructor
-        context = {
-            'instructor' : obj
-        }
+    context = {
+        'instructor' : obj
+    }
     return render(request, 'instructors.html', context=context)
+
+
